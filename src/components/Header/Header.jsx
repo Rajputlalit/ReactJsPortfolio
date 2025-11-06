50fec4db203f2870e4daadc88cfeab3c698b2370
DevL <161231154+Rajputlalit@users.noreply.github.com>
2025-11-06 16:09:27 +0530

    random

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,13 @@ import { NavLink } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import "./Header.css";
 import { FaEnvelope } from "react-icons/fa";
+// import logo from "../../assets/png/png/LC.png";
 
 function Header() {
   return (
     <header className='header'>
+        {/* <img src={logo} alt="Lalitcodes logo" className="mobile-logo" /> */}
+
       <h1><NavLink to="/" end>Lalitcodes.</NavLink></h1>
       <Navbar/>
       <NavLink to="/getintouch" >
